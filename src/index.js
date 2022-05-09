7fb7b05e6e8eaa8f75e453a56dd2b18ab46ca363
SWE1904867@xmu.edu.my <SWE1904867@xmu.edu.my>
2022-05-10 03:26:10 +0800

    Design for StudentHomepage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
