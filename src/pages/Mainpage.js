b6265249477c76b08794fbdd4c344e804c29b53c
SWE1904867@xmu.edu.my <SWE1904867@xmu.edu.my>
2022-06-24 00:52:49 +0800

    Implement server side data to the form and refactor some project structure for consistency

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -3,16 +3,16 @@ import { Navigate, useParams } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import SideBar from '../components/SideBar'
 import { LoginContext } from '../helper/Context'
-import HostelFunctions from "./hostelfunctions/HostelFunctions";
+import HostelFunctions from "./student/hostelfunctions/HostelFunctions";
 import Dashboard from "./Dashboard";
 import RequestMaintainence from "./RequestMaintainence";
 import ViewReport from "./ViewReport";
 import ProvideFeedback from "./ProvideFeedback";
 import Settings from "./settings/Settings";
-import ChangeHostelPeriod from "./hostelfunctions/ChangeHostelPeriod";
-import HostelApplication from "./hostelfunctions/HostelApplication";
-import RequestChangeRoom from "./hostelfunctions/RequestChangeRoom";
-import RequestRoomCheckout from "./hostelfunctions/RequestRoomCheckout";
+import ChangeHostelPeriod from "./student/hostelfunctions/ChangeHostelPeriod";
+import HostelApplication from "./student/hostelfunctions/HostelApplication";
+import RequestChangeRoom from "./student/hostelfunctions/RequestChangeRoom";
+import RequestRoomCheckout from "./student/hostelfunctions/RequestRoomCheckout";
 import './css/Mainpage.css'
 import Profile from './settings/Profile'
 import ChangePassword from './settings/ChangePassword'
--- src/pages/student/hostelfunctions/HostelApplication.js
+++ src/pages/student/hostelfunctions/HostelApplication.js
@@ -1,9 +1,79 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { LoginContext } from '../../../helper/Context'
 
 function HostelApplication() {
 
-  const[isAgreeTnC,setIsAgreeTnC] = useState(false)
+  const {loginDetails, setLoginDetails} = useContext(LoginContext);
+  const [activeUser, setActiveUser] = useState();
+  const [isAgreeTnC,setIsAgreeTnC] = useState(false);
+  const [nickname, setNickname] = useState('');
+  const [parentName1, setParentName1] = useState('');
+  const [parentName2, setParentName2] = useState('');
+  const [relationship1, setRelationship1] = useState('');
+  const [relationship2, setRelationship2] = useState('');
+  const [parentMobile1, setParentMobile1] = useState('');
+  const [parentMobile2, setParentMobile2] = useState('');
+  const [isSelectedBlockD, setIsSelectedBlockD] = useState(false);
+  const [isSelectedBlockLY, setIsSelectedBlockLY] = useState(false);
+  const [roommateName, setRoommateName] = useState('');
+  const [roommateNricPassport, setRoommateNricPassport] = useState('');
+  const [roommatePhoneNo, setRoommatePhoneNo] = useState('');
+  const [remarks, setRemarks] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/student')
+            .then(res => res.json())
+            .then(data => {
+              for(let i=0; i<data.length; i++){
+                if(loginDetails.user == data[i].student_id){
+                  setActiveUser(data[i]);
+                }
+              }
+            })
+  }, []);
+
+  function addApplicationRequest(isExistingStudent,studentId,fullname,nickname,nricPassport,gender,dob,nationality,
+    programme,enrollmentDate,email,phoneNo,address,parentName1,parentName2,relationship1,relationship2,parentMobile1,
+    parentMobile2,isSelectedBlockD,roommateName,roommatePhoneNo,roommateNricPassport,remarks){
+
+    fetch('http://localhost:8080/api/v1/applicationrequest',{
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/json'
+      },
+      body: JSON.stringify(
+          {
+            'existingStudent' : isExistingStudent,
+            'studentId': studentId,
+            'fullname': fullname,
+            'nickname': nickname,
+            'nricPassport': nricPassport,
+            'gender': gender,
+            'dob': dob,
+            'nationality': nationality,
+            'programme': programme,
+            'enrollmentDate': enrollmentDate,
+            'email': email,
+            'phoneNo': phoneNo,
+            'address': address,
+            'parentName1': parentName1,
+            'parentName2': parentName2,
+            'relationship1': relationship1,
+            'relationship2': relationship2,
+            'parentMobile1': parentMobile1,
+            'parentMobile2': parentMobile2,
+            'selectedBlockD': isSelectedBlockD,
+            'roommateName': roommateName,
+            'roommatePhoneNo': roommatePhoneNo,
+            'roommateNricPassport': roommateNricPassport,
+            'remarks': remarks
+          }
+      )
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
 
   return (
     <>
@@ -36,13 +106,13 @@ function HostelApplication() {
           <div className='d-flex row mt-3 align-items-end'>
             <p className='col-2 m-0'>Please tick (&#10003;):</p>
             <div className="col-2 form-check m-0">
-              <input className="form-check-input" type="checkbox"/>
+              <input className="form-check-input" type="checkbox" disabled/>
               <label className="form-check-label">
                 New Student
               </label>
             </div>
             <div className="col-2 form-check m-0">
-              <input className="form-check-input" type="checkbox"/>
+              <input className="form-check-input" type="checkbox" checked readOnly/>
               <label className="form-check-label">
                 Existing Student
               </label>
@@ -50,7 +120,15 @@ function HostelApplication() {
             <div className="d-flex col-4 row">
               <label className="col-3 col-form-label align-self-end p-0">Student ID:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.student_id
+                } 
+                disabled/>
               </div>
             </div>
           </div>
@@ -61,13 +139,21 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Full Name:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.fullname
+                } 
+                disabled/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">Nick Name:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value ={nickname} onChange={e => {setNickname(e.target.value)}}/>
               </div>
             </div>
           </div>
@@ -75,19 +161,37 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">NRIC/Passport No.:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.nric_passport
+                } 
+                disabled/>
               </div>
             </div>
             <div className="row col-6">
               <p className='col-4 m-0'>Gender:</p>
               <div className="col-3 form-check ms-3">
-                <input className="form-check-input" type="checkbox"/>
+                {(activeUser && activeUser.gender === 'Male')
+                ?
+                <input className="form-check-input" type="checkbox" checked readOnly/>
+                :
+                <input className="form-check-input" type="checkbox" disabled/>
+                }
                 <label className="form-check-label">
                   Male
                 </label>
               </div>
               <div className="col-3 form-check m-0">
-                <input className="form-check-input" type="checkbox"/>
+                {(activeUser && activeUser.gender === 'Female')
+                ?
+                <input className="form-check-input" type="checkbox" checked readOnly/>
+                :
+                <input className="form-check-input" type="checkbox" disabled/>
+                }
                 <label className="form-check-label">
                   Female
                 </label>
@@ -98,13 +202,29 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Date of Birth:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.dob
+                } 
+                disabled/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">Nationality:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.nationality
+                } 
+                disabled/>
               </div>
             </div>
           </div>
@@ -112,13 +232,29 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Programme:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.programme
+                } 
+                disabled/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-6 col-form-label">Enrollment Month and Year:</label>
               <div className="col-6">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.enrollmentDate
+                } 
+                disabled/>
               </div>
             </div>
           </div>
@@ -126,20 +262,44 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Email Address:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.email
+                } 
+                disabled/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">Mobile No.:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.phone_no
+                } 
+                disabled/>
               </div>
             </div>
           </div>
           <div className="row form-group mt-3">
             <label className="col-2 col-form-label px-0">Home Address:</label>
             <div className="col-10 ps-0">
-              <textarea rows='2' className="form-control"/>
+              <textarea rows='2' className="form-control" 
+                placeholder=
+                {(!activeUser)
+                  ?
+                  'na'
+                  :
+                  activeUser.address
+                } 
+                disabled/>
             </div>
           </div>
           <div className='row border mt-4 text-center'>
@@ -149,13 +309,13 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Name 01:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={parentName1} onChange={e => {setParentName1(e.target.value)}}/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">Name 02:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={parentName2} onChange={e => {setParentName2(e.target.value)}}/>
               </div>
             </div>
           </div>
@@ -163,13 +323,13 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Relationship:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={relationship1} onChange={e => {setRelationship1(e.target.value)}}/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">Relationship:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={relationship2} onChange={e => {setRelationship2(e.target.value)}}/>
               </div>
             </div>
           </div>
@@ -177,13 +337,13 @@ function HostelApplication() {
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Mobile No.:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={parentMobile1} onChange={e => {setParentMobile1(e.target.value)}}/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">Mobile No.:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={parentMobile2} onChange={e => {setParentMobile2(e.target.value)}}/>
               </div>
             </div>
           </div>
@@ -209,7 +369,13 @@ function HostelApplication() {
                 </tr>
                 <tr>
                     <td className='text-center'>
-                      <input className="form-check-input" type="checkbox"/>
+                      {isSelectedBlockLY
+                      ?
+                      <input className="form-check-input" type="checkbox" value={isSelectedBlockD} onChange={() => {setIsSelectedBlockD(!isSelectedBlockD)}} disabled/>
+                      :
+                      <input className="form-check-input" type="checkbox" value={isSelectedBlockD} onChange={() => {setIsSelectedBlockD(!isSelectedBlockD)}}/>
+                      }
+                      
                     </td>
                     <td className='text-center'>Twin (Block D)</td>
                     <td className='text-center'>RM340</td>
@@ -217,7 +383,13 @@ function HostelApplication() {
                 </tr>
                 <tr>
                     <td className='text-center'>
-                      <input className="form-check-input" type="checkbox"/>
+                      {isSelectedBlockD
+                      ?
+                      <input className="form-check-input" type="checkbox" value={isSelectedBlockLY} onChange={() => {setIsSelectedBlockLY(!isSelectedBlockLY)}} disabled/>
+                      :
+                      <input className="form-check-input" type="checkbox" value={isSelectedBlockLY} onChange={() => {setIsSelectedBlockLY(!isSelectedBlockLY)}}/>
+                      }
+                      
                     </td>
                     <td className='text-center'>Twin (Block LY)</td>
                     <td className='text-center'>RM390</td>
@@ -232,27 +404,27 @@ function HostelApplication() {
           <div className="row mt-3">
             <label className="col-3 col-form-label ps-0">Name of Roomate (if applicable):</label>
             <div className="col-9">
-              <input type="text" className="form-control"/>
+              <input type="text" className="form-control" value={roommateName} onChange={e => {setRoommateName(e.target.value)}}/>
             </div>
           </div>
           <div className='row form-group mt-3'>
             <div className="row col-6 ps-0">
               <label className="col-4 col-form-label">Roomate Mobile No.:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={roommatePhoneNo} onChange={e => {setRoommatePhoneNo(e.target.value)}}/>
               </div>
             </div>
             <div className="row col-6">
               <label className="col-4 col-form-label">NRIC/Passport No.:</label>
               <div className="col-8">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={roommateNricPassport} onChange={e => {setRoommateNricPassport(e.target.value)}}/>
               </div>
             </div>
           </div>
           <div className="row mt-3">
             <label className="col-3 col-form-label ps-0">Remark(s):</label>
             <div className="col-9">
-              <input type="text" className="form-control"/>
+              <input type="text" className="form-control" value={remarks} onChange={e => {setRemarks(e.target.value)}}/>
             </div>
           </div>
           <div className='row border mt-4 text-center'>
@@ -298,7 +470,14 @@ function HostelApplication() {
           {isAgreeTnC 
           ? <div className="form-group row">
               <div className="col-12 mt-4">
-                <button type="submit" className="btn btn-primary float-end">Submit Form</button>
+                <button type="submit" className="btn btn-primary float-end" 
+                onClick={() => {
+                  addApplicationRequest(true,activeUser.student_id,activeUser.fullname,nickname,activeUser.nric_passport,activeUser.gender,activeUser.dob,
+                    activeUser.nationality,activeUser.programme,activeUser.enrollmentDate,activeUser.email,activeUser.phone_no,activeUser.address,parentName1,
+                    parentName2,relationship1,relationship2,parentMobile1,parentMobile2,isSelectedBlockD,roommateName,roommatePhoneNo,roommateNricPassport,remarks);
+                    alert('Response received and stored.\n\nKindly wait for several operation day for the approval or rejection of the submitted request.')
+                  }}>
+                  Submit Form</button>
               </div>
             </div>
           : <div className="row">
